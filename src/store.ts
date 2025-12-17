a113e96bed76f8b3490f5f845a6db214c1aead47
eritque0arcus <eritque-arcus@ikuyo.dev>
2025-12-16 20:16:28 -0600

    chore: clean unused val

--- src/store.ts
+++ src/store.ts
@@ -3,6 +3,4 @@ import { atom } from "nanostores";
 
 export const theme = persistentBoolean("theme"); // false for light, true for dark
 
-export const currentIndex = atom(0);
-
 export const isZoomed = atom(false);
